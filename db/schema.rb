5800d7bf401b6afcf341b9d3d6f9063b9234cd4a
ihortok <itokaryk@gmail.com>
2021-10-22 21:42:38 +0300

    authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_145558) do
+ActiveRecord::Schema.define(version: 2021_10_22_182153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "original_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+unless User.exists? email: 'test@test.com'
+  User.create(
+    email: 'test@test.com',
+    password: '123456abc@',
+    password_confirmation: '123456abc@'
+  )
+
+  p 'user test@test.com - created'
+end
+
+authors = ['Jules Verne', 'Jack London', 'Dan Brown']
+
+authors.each do |author_name|
+  next if Author.exists? name: author_name
+
+  Author.create(name: author_name)
+
+  p "author #{author_name} - created"
+end
