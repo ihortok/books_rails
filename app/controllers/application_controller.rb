302b2754130b3d5850a9ef6ac7726468abd500dc
ihortok <itokaryk@gmail.com>
2021-10-31 20:22:32 +0200

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
   private
@@ -14,6 +15,11 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :locale])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :locale])
   end
 
   def set_locale
