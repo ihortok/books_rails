237109fd25cab91a904b65d4047eb55efb07c69c
ihortok <itokaryk@gmail.com>
2021-11-13 00:34:50 +0200

    storage dir to linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:ihortok/books_rails.git'
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads', 'storage'
 
 set :keep_releases, 1
 
