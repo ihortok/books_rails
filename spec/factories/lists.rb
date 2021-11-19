9de0dda8618249d1b7b559d8bfe5c33348ddd9e9
ihortok <itokaryk@gmail.com>
2021-11-19 23:13:32 +0200

    factories

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :list do
-    title { "MyString" }
-    user { nil }
+    user
+    title { Faker::Hipster.word }
+
+    trait :with_books do
+      after(:create) do |list|
+        books = FactoryBot.create_list(:book, 3)
+
+        list.books << books
+      end
+    end
   end
 end
