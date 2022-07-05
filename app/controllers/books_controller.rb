6a11c9b2852af08fc8c23a69ea46e171a22fe323
yuliatokaryk <julia4099@gmail.com>
2022-07-05 16:47:10 +0300

    year of publication added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,7 +64,7 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title, :original_title, :description, :author_id, :goodreads_url, :image)
+    params.require(:book).permit(:title, :original_title, :description, :author_id, :goodreads_url, :image, :published)
   end
 
   def book_update_params
